db0ff63252b0917645a42a89f8ea4ffb07c15eb4
Kirill Kuyan <progingisfun@gmail.com>
2019-10-06 22:35:00 +0300

    Create project's base
    Create application's base

---
+++ src/main/java/university/project/roomserviceondemand/application/RoomserviceondemandApplication.java
@@ -0,0 +1,13 @@
+package university.project.roomserviceondemand.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomserviceondemandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoomserviceondemandApplication.class, args);
+	}
+
+}
