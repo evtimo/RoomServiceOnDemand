47e7c668637f695c6c2f08a401afae43fbb26571
Kirill Kuyan <progingisfun@gmail.com>
2019-10-21 16:21:39 +0300

    Changed HomeController
    Changed mapping

--- src/main/java/university/project/roomserviceondemand/controllers/HomeController.java
+++ src/main/java/university/project/roomserviceondemand/controllers/HomeController.java
@@ -6,11 +6,13 @@ package university.project.roomserviceondemand.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/home")
 public class HomeController {
 
-    @GetMapping("/home")
+    @GetMapping("/requests")
     public String home(){
         return "requests";
     }
