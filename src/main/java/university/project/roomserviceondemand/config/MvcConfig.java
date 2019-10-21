ac5b71d97c3221ddd8e294dd5388a237d9bc97f4
Kirill Kuyan <progingisfun@gmail.com>
2019-10-21 04:50:50 +0300

    Changed MvcConfig
    Added resolver path for signIn page

--- src/main/java/university/project/roomserviceondemand/config/MvcConfig.java
+++ src/main/java/university/project/roomserviceondemand/config/MvcConfig.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -18,5 +19,10 @@ public class MvcConfig implements WebMvcConfigurer {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/**")
                 .addResourceLocations("classpath:/static/");
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/signIn").setViewName("signin");
     }
 }
