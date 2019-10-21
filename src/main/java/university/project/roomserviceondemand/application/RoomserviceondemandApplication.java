7d16611a82803f0dd69c419522a0aa441cf04e98
Kirill Kuyan <progingisfun@gmail.com>
2019-10-21 04:28:47 +0300

    Changed Boot application class
    Added configuration for components scan

--- src/main/java/university/project/roomserviceondemand/application/RoomserviceondemandApplication.java
+++ src/main/java/university/project/roomserviceondemand/application/RoomserviceondemandApplication.java
@@ -2,8 +2,13 @@ package university.project.roomserviceondemand.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"university.project.roomserviceondemand"})
+@EntityScan(basePackages = "university.project.roomserviceondemand.models")
 public class RoomserviceondemandApplication {
 
 	public static void main(String[] args) {
