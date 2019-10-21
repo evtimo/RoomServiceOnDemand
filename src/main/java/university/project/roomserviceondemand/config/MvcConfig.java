eae233f2fe6f72acb33e37d632e25340be22c640
Kirill Kuyan <progingisfun@gmail.com>
2019-10-21 04:27:50 +0300

    Added MVC Config
    Register static content

---
+++ src/main/java/university/project/roomserviceondemand/config/MvcConfig.java
@@ -0,0 +1,22 @@
+package university.project.roomserviceondemand.config;
+/* Created by Kuyan Kirill
+ *  Email: progingisfun@gmail.com
+ *  Date: 21.10.2019
+ */
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class MvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+                .addResourceLocations("classpath:/static/");
+    }
+}
