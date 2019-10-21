d1dbcdbb1b9645b62cf8fe928f07abe157e3ae66
Kirill Kuyan <progingisfun@gmail.com>
2019-10-21 16:26:02 +0300

    Added additional configuration
    Added additional configuration in boot application class

--- src/main/java/university/project/roomserviceondemand/application/RoomserviceondemandApplication.java
+++ src/main/java/university/project/roomserviceondemand/application/RoomserviceondemandApplication.java
@@ -6,9 +6,11 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+
 @ComponentScan(basePackages = {"university.project.roomserviceondemand"})
 @EntityScan(basePackages = "university.project.roomserviceondemand.models")
+@EnableJpaRepositories(basePackages = "university.project.roomserviceondemand.repository")
+@SpringBootApplication
 public class RoomserviceondemandApplication {
 
 	public static void main(String[] args) {
