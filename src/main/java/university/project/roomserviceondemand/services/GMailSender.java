d596d075e623d375c2b83f101cfa42708ced2b09
Kirill Kuyan <progingisfun@gmail.com>
2019-10-22 03:03:35 +0300

    Added GMailService
    Added GMailService for email sending

---
+++ src/main/java/university/project/roomserviceondemand/services/GMailSender.java
@@ -0,0 +1,35 @@
+package university.project.roomserviceondemand.services;
+/* Created by Kuyan Kirill
+ *  Email: progingisfun@gmail.com
+ *  Date: 22.10.2019
+ */
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.stereotype.Service;
+import university.project.roomserviceondemand.utils.MailSender;
+
+@Service
+public class GMailSender implements MailSender {
+
+    private final JavaMailSender javaMailSender;
+
+    public GMailSender(@Qualifier("getJavaGMailSender") JavaMailSender javaMailSender) {
+        this.javaMailSender = javaMailSender;
+    }
+
+    @Override
+    public void send(String from, String to, String text) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom(from);
+        message.setTo(to);
+        message.setSubject("You get feedback");
+        message.setText(text);
+
+        javaMailSender.send(message);
+
+    }
+}
