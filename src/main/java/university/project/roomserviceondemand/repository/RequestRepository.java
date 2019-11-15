58f5f3cf3686376d3fd471580e5617df1ba84d33
Evgenii <zheny.tim@gmail.com>
2019-11-15 22:47:10 +0300

    Add changes in implementation

--- src/main/java/university/project/roomserviceondemand/repository/RequestRepository.java
+++ src/main/java/university/project/roomserviceondemand/repository/RequestRepository.java
@@ -9,4 +9,5 @@ import java.util.List;
 @Repository
 public interface RequestRepository extends JpaRepository<Request, Long> {
     List<Request> getAllByUserId(Long userId);
+    void deleteAll();
 }
