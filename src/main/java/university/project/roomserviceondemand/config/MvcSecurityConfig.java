0c4eeab3673c38970b1203e11eedc39bd846cb80
Firstname Lastname <zheny.tim@gmail.com>
2019-11-15 22:39:18 +0300

    Fix wrong import

--- src/main/java/university/project/roomserviceondemand/config/MvcSecurityConfig.java
+++ src/main/java/university/project/roomserviceondemand/config/MvcSecurityConfig.java
@@ -4,7 +4,6 @@ package university.project.roomserviceondemand.config;
  *  Date: 21.10.2019
  */
 
-import com.sun.javafx.sg.prism.NodePath;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
